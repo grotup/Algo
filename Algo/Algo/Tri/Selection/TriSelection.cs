7c15712aba01e8601eefa6f62eb9dede5141878f
Nb <nael.sylvain@gmail.com>
2014-10-19 20:33:31 +0200

    Implémentation tri par sélection

---
+++ Algo/Algo/Tri/Selection/TriSelection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo.Tri.Selection
+{
+    public class TriSelection
+    {
+        public static List<int> Trier(List<int> Liste)
+        {
+            int Min = 0;
+            int Tmp;
+            for (int i = 0; i < Liste.Count; i++)
+            {
+                Min = i;
+                for (int j = i; j < Liste.Count; j++)
+                {
+                    if (Liste[j] < Liste[Min])
+                        Min = j;
+                }
+                if (Min != i)
+                {
+                    Tmp = Liste[i];
+                    Liste[i] = Liste[Min];
+                    Liste[Min] = Tmp;
+                }
+            }
+            return Liste;
+        }
+    }
+}
---
+++ Algo/AlgoTest/Tri/Selection/TriSelectionTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+using Algo.Utils;
+using Algo.Tri.Selection;
+
+namespace AlgoTest.Tri.Selection
+{
+    [TestClass]
+    public class TriSelectionTest
+    {
+        [TestMethod]
+        public void TrierTestSelection()
+        {
+            List<int> ListeTest = Utils.GenerateNumbers(10);
+            ListeTest = TriSelection.Trier(ListeTest);
+            Assert.IsTrue(Utils.IsSorted(ListeTest));
+        }
+    }
+}
